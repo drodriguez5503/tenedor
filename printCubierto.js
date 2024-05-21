a5ff497b0e67a353624032f2b8934735b27470f2
MichaelVahos <michaelvahos2002@gmail.com>
2024-05-21 12:42:48 +0200

    implementacion de metodos para cada cubierto

--- printCubierto.js
+++ printCubierto.js
@@ -1,9 +1,47 @@
-console.log("l l l");
-console.log("l l l");
-console.log("l l l");
-console.log("l_l_l");
-console.log("  l ");
-console.log("  l ");
-console.log("  l ");
-console.log("  l");
-console.log("Michael estuvo aqui")
+const input = require("prompt-sync")();
+
+let respuesta;
+do {
+    respuesta = input("Ingresa un cubierto - tenedor, cuchara, cuchillo o salir: ");
+
+    if (respuesta === "tenedor") {
+        tenedor();
+    } else if (respuesta === "cuchara") {
+        cuchara();
+    } else if (respuesta === "cuchillo") {
+        cuchillo();
+    }
+} while (respuesta !== "salir");
+
+function tenedor() {
+    console.log("l l l");
+    console.log("l l l");
+    console.log("l l l");
+    console.log("l_l_l");
+    console.log("  l ");
+    console.log("  l ");
+    console.log("  l ");
+    console.log("  l");
+}
+
+function cuchara() {
+    console.log("_____");
+    console.log("l   l");
+    console.log("l   l");
+    console.log("l___l");
+    console.log("  l ");
+    console.log("  l ");
+    console.log("  l ");
+    console.log("  l");   
+}
+
+function cuchillo() {
+    console.log("   /l");
+    console.log("  / l");
+    console.log(" /  l");
+    console.log("/___l");
+    console.log("  l l");
+    console.log("  l l");
+    console.log("  l l");
+    console.log("  l_l");   
+}
